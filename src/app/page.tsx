42038ac523f1269b3a9f435e09745594a40ea512
Au = Or[79]² <assanisaidelamani@gmail.com>
2025-07-29 16:05:49 +0200

    [EDIT] - the mobile navigation bar has been fixed at the top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Navigation from "@/components/layout/home/navigation";
 export default function Home() {
   return (
     <div>
-      <div className="md:hidden mb-5 flex items-center justify-center w-full bg-gray-200 dark:bg-[#282829] pt-2 pb-2 rounded-bl-2xl rounded-br-2xl">
+      <div className="fixed top-0 md:hidden mb-5 flex items-center justify-center w-full bg-gray-200 dark:bg-[#282829] pt-2 pb-2 rounded-bl-2xl rounded-br-2xl">
         <Navigation />
       </div>
       <div className="flex flex-col md:flex-row w-full gap-10">
