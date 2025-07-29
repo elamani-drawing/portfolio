d6f5e36243e8b2063c7a6a65a2858e04ef06c5cf
Au = Or[79]² <assanisaidelamani@gmail.com>
2025-07-29 23:55:11 +0200

    [FIX] - ProfileCard have an auto height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="fixed top-0 md:hidden mb-5 flex items-center justify-center w-full bg-gray-200 dark:bg-[#282829] pt-2 pb-2 rounded-bl-2xl rounded-br-2xl">
         <Navigation />
       </div>
-      <div className="flex flex-col md:flex-row w-full gap-10">
+      <div className="flex flex-col items-start md:flex-row w-full gap-10">
         <ProfileCard />
         <div className="w-full md:w-3/4 relative">
           <MainContainer />
