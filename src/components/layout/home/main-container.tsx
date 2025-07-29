2d7a842de6870ecc1164ca2f458aa2b1ebea796a
Au = Or[79]² <assanisaidelamani@gmail.com>
2025-07-29 23:51:00 +0200

    [ADD] - description in main-container, 'ce que je fais' in main-container, skill-card.tsx

--- src/components/layout/home/main-container.tsx
+++ src/components/layout/home/main-container.tsx
@@ -1,5 +1,7 @@
 import { Card, CardHeader } from "@/components/ui/card";
 import Navigation from "@/components/layout/home/navigation";
+import { Smartphone, Code, Gamepad2, Database } from 'lucide-react'
+import { SkillCard } from "./skill-card";
 
 export default function MainContainer() {
     return (
@@ -11,9 +13,63 @@ export default function MainContainer() {
                         <Navigation />
                     </div>
                 </div>
-            <div className="w-16 h-1 bg-yellow-600 dark:bg-yellow-500 mb-6"></div>
+                <div className="w-16 h-1 bg-yellow-600 dark:bg-yellow-500 mb-6"></div>
             </CardHeader>
-            <div className="p-5">contenu</div>
+            <div className="p-5 pt-0">
+                <div className="mb-8">
+                    <p className="mb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit sed quod eveniet. Iusto, dolor! Accusantium debitis velit sed possimus laudantium dolores tenetur doloremque voluptatum sunt perspiciatis? Facere pariatur distinctio enim.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi molestiae dolores voluptas corporis dolore harum eos, ratione est repellendus ut sint cumque nisi tempore magni maiores nulla illo quae provident.
+                    </p>
+                    <p className="mb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit sed quod eveniet. Iusto, dolor! Accusantium debitis velit sed possimus laudantium dolores tenetur doloremque voluptatum sunt perspiciatis? Facere pariatur distinctio enim.</p>
+                    <p className="mb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit sed quod eveniet. Iusto, dolor! Accusantium debitis velit sed possimus laudantium dolores tenetur doloremque voluptatum sunt perspiciatis? Facere pariatur distinctio enim.
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit ut officiis illo nam repellat quo temporibus perspiciatis totam harum odio deleniti neque in tempora omnis, nulla incidunt explicabo inventore et.
+                    </p>
+                </div>
+                <div>
+
+                    <h2 className="text-3xl font-bold mb-6">Ce Que Je Fais</h2>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                        <SkillCard
+                            icon={
+                                <Smartphone
+                                    className="text-yellow-600 dark:text-yellow-500"
+                                    size={28}
+                                />
+                            }
+                            title="Développement D'Applications"
+                            description="Développement d'applications en Java et C#, en mettant l'accent sur la robustesse, la maintenabilité et une excellente expérience utilisateur."
+                        />
+                        <SkillCard
+                            icon={
+                                <Code className="text-yellow-600 dark:text-yellow-500" size={28} />
+                            }
+                            title="Développement Web"
+                            description="Création de sites web performants et adaptés aux besoins des utilisateurs, avec une attention particulière à l'optimisation et à l'ergonomie."
+                        />
+                        <SkillCard
+                            icon={
+                                <Gamepad2
+                                    className="text-yellow-600 dark:text-yellow-500"
+                                    size={28}
+                                />
+                            }
+                            title="Développement De Jeux Vidéo"
+                            description="Conception et développement de jeux interactifs avec Unity, en combinant créativité et technologies avancées."
+                        />
+                        <SkillCard
+                            icon={
+                                <Database
+                                    className="text-yellow-600 dark:text-yellow-500"
+                                    size={28}
+                                />
+                            }
+                            title="Gestion De Bases De Données (SGBD)"
+                            description="Conception, gestion et optimisation de bases de données, principalement SQL et NoSQL."
+                        />
+                    </div>
+                </div>
+            </div>
+
         </Card>
     );
 }
---
+++ src/components/layout/home/skill-card.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from 'react'
+interface SkillCardProps {
+  icon: ReactNode
+  title: string
+  description: string
+}
+export const SkillCard = ({ icon, title, description }: SkillCardProps) => {
+  return (
+    <div className="bg-gray-100 dark:bg-[#1a1a1a] p-6 rounded-lg flex shadow-sm">
+      <div className="mr-4 mt-1">{icon}</div>
+      <div>
+        <h3 className="text-xl font-bold mb-2">{title}</h3>
+        <p className="text-gray-600 dark:text-gray-400">{description}</p>
+      </div>
+    </div>
+  )
+}
