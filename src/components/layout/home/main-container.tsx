55822c32210a412b058d41e15523bf4bbc49bdcc
Au = Or[79]² <assanisaidelamani@gmail.com>
2025-07-30 03:04:36 +0200

    [EDIT] - Synchronization of the page title, when the navigation changes the title is updated

--- src/components/layout/home/main-container.tsx
+++ src/components/layout/home/main-container.tsx
@@ -6,7 +6,7 @@ import ResumeContent from "@/components/layout/home/resume-content";
 function renderSection(section: string) {
   switch (section.toLocaleLowerCase()) {
     // case "about":
-    //   return <HomeContent />;
+    //   return <HomeContent />; 
     case "resume":
       return <ResumeContent />;
     case "projects":
@@ -17,13 +17,27 @@ function renderSection(section: string) {
       return <HomeContent />;
   }
 }
+  const getTitle = (section: string) => {
+    switch (section) {
+    //   case "about":
+    //     return "Mon Profil";
+      case "resume":
+        return "Mon CV";
+      case "projects":
+        return "Mes Projets";
+      case "contact":
+        return "Contactez-moi";
+      default:
+        return "Mon Profil";
+    }
+  };
 
 export default function MainContainer({ onSelect, activeSection }: NavigationProps) {
     return (
         <Card className="p-0">
             <CardHeader className="relative flex flex-col p-0 pl-5">
                 <div className="relative flex pl-0 w-full">
-                    <h1 className="text-3xl font-bold pl-0 pt-5 pb-5">Mon Profil</h1>
+                    <h1 className="text-3xl font-bold pl-0 pt-5 pb-5">{getTitle(activeSection)}</h1>
                     <div className="hidden md:flex justify-end md:absolute md:w-3/4 lg:w-2/4 top-0 right-0 bg-gray-200 dark:bg-[#282829]  border-l border-b border-none dark:border-black rounded-bl-[250px] rounded-tr-[65px] shadow-md p-2">
                         <Navigation onSelect={onSelect} activeSection={activeSection} />
                     </div>
